5c9617fc2816471981901a560877d7d7941c021b
Kushal <sharmaje360@gmail.com>
2022-02-01 22:46:51 +0530

    Adding a top loading bar to NewsStar

--- src/componets/News.js
+++ src/componets/News.js
@@ -28,15 +28,18 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4e30a1f4471342deb1c3bc88646597b8&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(50);
     let parseData = await data.json();
     this.setState({
       articles: parseData.articles,
       totalResults: parseData.totalResults,
       loading: false
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
